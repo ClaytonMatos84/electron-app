0987382d6f499476166b03f6da729d1cd1c50cbf
cmatos <cmatos@uolinc.com>
2025-04-01 19:55:54 -0300

    [master] initial commit

---
+++ app/public/js/renderer.js
@@ -0,0 +1,9 @@
+const information = document.getElementById('info');
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+const pingPong = async () => {
+    const response = await versions.ping();
+    information.innerText += `\nPing: ${response}`;
+};
+
+pingPong();
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            preload: `${__dirname}/app/js/preload.js`,
+        },
+    });
+
+    win.loadURL(`file://${__dirname}/app/index.html`);
+};
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong');
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
